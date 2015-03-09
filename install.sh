4fd646c601444c51d2e08d09d116d98759ca8db3
Ian Miell <ian.miell@gmail.com>
2015-03-09 18:27:32 +0000

    updates

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -o errexit
 apt-get update
 apt-get upgrade -y
-apt-get install -y git wget sudo build-essential
+apt-get install -y git wget sudo
 git clone https://github.com/facebook/osquery.git
 cd /osquery
 git checkout 1.0.3
@@ -10,9 +10,9 @@ git checkout 1.0.3
 make
 make package
 dpkg -i ./build/linux/osquery-0.0.1-trusty.amd64.deb
-apt-get purge git wget sudo build-essential
+SUDO_FORCE_REMOVE=yes apt-get purge -y git wget sudo
 rm -rf /var/lib/apt/lists/*
-apt-get autoremove
+apt-get autoremove -y
 apt-get clean
 cd /
 rm -rf /osquery
